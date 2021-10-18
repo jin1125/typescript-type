a14f51d4d1b880c5754c48236484d3ce78052f16
jin1125 <shogo.jinta@gmail.com>
2021-10-18 16:26:45 +0900

    これがTypeScriptの構造的部分型だ！

--- dist/interface.js
+++ dist/interface.js
@@ -5,6 +5,15 @@ class Developer {
         this.age = age;
     }
     greeting(message) {
-        console.log('Hello');
+        console.log(message);
     }
 }
+const tmpDeveloper = {
+    name: 'Quell',
+    age: 38,
+    experience: 3,
+    greeting(message) {
+        console.log(message);
+    }
+};
+const user = tmpDeveloper;
--- src/interface.ts
+++ src/interface.ts
@@ -7,7 +7,18 @@ interface Human {
 class Developer implements Human {
   constructor(public name:string,public age:number,experience:number){}
   greeting(message:string){
-    console.log('Hello');
+    console.log(message);
+  }
+}
+
+const tmpDeveloper =  {
+  name: 'Quell',
+  age: 38,
+  experience:3,
+  greeting(message:string){
+    console.log(message);
     
   }
 }
+
+const user : Human = tmpDeveloper
