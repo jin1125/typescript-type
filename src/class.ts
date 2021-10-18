3c43db9cae13360c6af049ee41d2559d7b8a007f
jin1125 <shogo.jinta@gmail.com>
2021-10-18 11:04:30 +0900

    public修飾子とprivate修飾子を使用して、アクセスを制限する方法

--- src/class.ts
+++ src/class.ts
@@ -1,22 +1,23 @@
 class Person {
-  name:string;
-  constructor(initName:string){
+
+  public name:string;
+  private age:number;
+  
+  constructor(initName:string,initAge:number){
     this.name = initName;
+    this.age = initAge;
+  }
+
+  incrementAge(){
+    this.age += 1;
   }
 
   greeting(this:Person){
-    console.log(`hello! my name is ${this.name}`);
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
     
   }
 }
 
-const quill =  new Person("Quill");
+const quill =  new Person("Quill",38);
+quill.incrementAge();
 quill.greeting();
-
-const anotherQuill ={
-  name:'anotherQuill',
-  greeting(){},
-  anotherGreeting:quill.greeting
-}
-
-anotherQuill.anotherGreeting();
