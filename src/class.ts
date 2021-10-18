30eebeabee3988264bcd6194ee15378b99b9cfb1
jin1125 <shogo.jinta@gmail.com>
2021-10-18 13:18:37 +0900

    privateをconstructorに付けて、シングルトンパターンを実装する方法

--- src/class.ts
+++ src/class.ts
@@ -25,6 +25,7 @@ abstract class Person {
 
 
 class Teacher extends Person{
+  private static instance:Teacher;
   explainJob(){
     console.log(`I am a teacher and I teach ${this.subject}.`);
   }
@@ -35,9 +36,6 @@ class Teacher extends Person{
     }
     return this._subject
   }
-  constructor(name: string, age: number,private _subject:string){
-    super(name, age);
-  }
 
   set subject(value){
     if(!value){
@@ -45,7 +43,19 @@ class Teacher extends Person{
     }
     this._subject = value
   }
+  
+  private constructor(name: string, age: number,private _subject:string){
+    super(name, age);
+  }
 
+  static getInstance(){
+    if(Teacher.instance) return Teacher.instance;
+      Teacher.instance =  new Teacher('Quill',38,'Math');
+      return Teacher.instance;
+  }
 }
-const teacher = new Teacher('Quill',38,'Math');
-teacher.greeting()
+
+const teacher = Teacher.getInstance();
+const teacher2 = Teacher.getInstance();
+console.log(teacher,teacher2);
+
