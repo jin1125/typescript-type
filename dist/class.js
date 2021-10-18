df1e9ab51cbc7080aaa67baa2381ebb35348c7eb
jin1125 <shogo.jinta@gmail.com>
2021-10-18 11:33:39 +0900

    extendsを使用して、他のクラスの機能を継承する方法

--- dist/class.js
+++ dist/class.js
@@ -11,6 +11,14 @@ class Person {
         console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
     }
 }
-const quill = new Person("Quill", 38);
-quill.incrementAge();
-quill.greeting();
+class Teacher extends Person {
+    constructor(name, age, subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+    greeting() {
+        console.log(`Hello! My name is ${this.name}. I am ${this.age} years old. I teach ${this.subject}`);
+    }
+}
+const teacher = new Teacher('Quill', 38, 'Math');
+teacher.greeting();
--- src/class.ts
+++ src/class.ts
@@ -1,6 +1,6 @@
 class Person {
   
-  constructor(public readonly name:string, private age:number){
+  constructor(public name:string, private age:number){
   }
 
   incrementAge(){
@@ -13,6 +13,17 @@ class Person {
   }
 }
 
-const quill =  new Person("Quill",38);
-quill.incrementAge();
-quill.greeting();
+
+class Teacher extends Person{
+  constructor(name: string, age: number,public subject:string){
+    super(name, age);
+  }
+
+  greeting(){
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old. I teach ${this.subject}`);
+    
+  }
+}
+
+const teacher = new Teacher('Quill',38,'Math');
+teacher.greeting();
