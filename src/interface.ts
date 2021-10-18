084bc8128c3c03777c4d09afcc54e423ca1647fc
jin1125 <shogo.jinta@gmail.com>
2021-10-18 17:13:40 +0900

    extendsを使ってinterfaceを継承する方法

--- src/interface.ts
+++ src/interface.ts
@@ -1,10 +1,13 @@
-interface Human {
-  readonly name: string;
+interface Nameable{
+  name:string;
+}
+
+interface Human extends Nameable {
   age: number;
   greeting(message:string):void
 }
 
-class Developer implements Human {
+class Developer implements Human{
   constructor(public name:string,public age:number,experience:number){}
   greeting(message:string){
     console.log(message);
